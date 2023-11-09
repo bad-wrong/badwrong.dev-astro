3175ecd882555490fb1a82dc23a801813641751c
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-11-10 00:55:24 +0200

    Refactor Header

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,9 @@
+export default function HeaderBar() {
+  return (
+    <div className="p-8">
+      <a href="/" title="Go to Home">
+        <img src="/images/B&R_Logotype.svg" alt="Bad & Wrong Logotype" />
+      </a>
+    </div>
+  );
+}
