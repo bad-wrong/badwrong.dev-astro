9c4273508bb4f7914d6a85af68aa2f2291ea4b4d
Ihar Suvorau <ihar.suvorau@gmail.com>
2025-03-30 20:16:50 +0300

    Upgrade Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://badwrong.dev",
+
   integrations: [
     vue(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
     react(),
   ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
